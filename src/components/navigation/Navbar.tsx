09133945fc128eac282cc538183fa5bb5d36e067
Vitor <marthendalnunes@gmail.com>
2022-11-14 19:40:57 -0300

    feat: add wallet connection

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react'
 import Link from 'next/link'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+
 
 export const Navbar: FC = () => {
   return (
-    <nav className="sticky top-0 bg-white h-24 w-full flex items-center px-8 shadow-md">
+    <nav className="sticky top-0 bg-white h-28 w-full flex justify-between items-center px-12 shadow-md">
       <Link href="/">
         <div className="font-semibold text-xl cursor-pointer">Budget Boxes</div>
       </Link>
+      <ConnectButton />
     </nav>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,48 @@
+import '@rainbow-me/rainbowkit/styles.css'
 import '../styles/globals.css'
 import { ApolloProvider } from '@apollo/client'
-import { graphqlClient } from '@/graphql/clients/client'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { infuraProvider } from 'wagmi/providers/infura'
 import { Navbar } from '@/components/navigation/Navbar'
-
+import { graphqlClient } from '@/graphql/clients/client'
 import type { AppProps } from 'next/app'
+
+const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string
+const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY as string
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet],
+  [
+    alchemyProvider({ apiKey: ALCHEMY_API_KEY }),
+    infuraProvider({ apiKey: INFURA_API_KEY })
+  ]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Budget Boxes',
+  chains
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={graphqlClient}>
-      <Navbar />
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        
+          <Navbar />
+          <Component {...pageProps} />
+        
+      </RainbowKitProvider>
+    </WagmiConfig>
     </ApolloProvider>
   )
 }
