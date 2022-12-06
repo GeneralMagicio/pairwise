50a31e02c8f19902181241f6cd475f1acced2f4b
Vitor <marthendalnunes@gmail.com>
2022-12-06 10:44:06 -0300

    feat: add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { createProxySSGHelpers } from '@trpc/react-query/ssg'
+import Head from 'next/head'
 import superjson from '@/utils/superjson'
 import { appRouter } from '@/server/trpc/router/_app'
 import { trpc } from '@/utils/trpc'
@@ -26,13 +27,18 @@ const Home = () => {
     refetchOnWindowFocus: false
   })
   return (
-    <main className="px-4 md:px-14">
-      <div className="grid justify-items-center gap-y-8 px-4 pt-16 sm:px-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 ">
-        {budgetBoxes?.map((budgetBox) => (
-          <BudgetBoxCard key={budgetBox.id} budgetBox={budgetBox} />
-        ))}
-      </div>
-    </main>
+    <div>
+      <Head>
+        <title>Budget Boxes</title>
+      </Head>
+      <main className="px-4 md:px-14">
+        <div className="grid justify-items-center gap-y-8 px-4 pt-16 sm:px-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 ">
+          {budgetBoxes?.map((budgetBox) => (
+            <BudgetBoxCard key={budgetBox.id} budgetBox={budgetBox} />
+          ))}
+        </div>
+      </main>
+    </div>
   )
 }
 
