706421e00cbebf11a6857ad92cf16af3973ab59c
Vitor <marthendalnunes@gmail.com>
2022-12-12 22:39:33 -0300

    fix: invalid elements on icons

--- src/components/icons/CreateIcon.tsx
+++ src/components/icons/CreateIcon.tsx
@@ -14,10 +14,8 @@ export const CreateIcon: FC<IconProps> = ({
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      clip-rule="evenodd"
       d="M12 0a2.4 2.4 0 0 1 2.4 2.4v7.2h7.2a2.4 2.4 0 0 1 0 4.8h-7.2v7.2a2.4 2.4 0 0 1-4.8 0v-7.2H2.4a2.4 2.4 0 0 1 0-4.8h7.2V2.4A2.4 2.4 0 0 1 12 0Z"
       fill={color}
-      fill-rule="evenodd"
     />
   </svg>
 )
