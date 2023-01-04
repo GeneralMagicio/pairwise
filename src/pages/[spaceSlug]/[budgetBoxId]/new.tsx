5930afa4f6134001df84da9f5407058378895841
Vitor <marthendalnunes@gmail.com>
2023-01-04 11:29:20 -0300

    feat: add navarrow to registration pages

--- src/pages/[spaceSlug]/[budgetBoxId]/new.tsx
+++ src/pages/[spaceSlug]/[budgetBoxId]/new.tsx
@@ -1,12 +1,88 @@
+import { createProxySSGHelpers } from '@trpc/react-query/ssg'
 import Head from 'next/head'
+import superjson from '@/utils/superjson'
+import { appRouter } from '@/server/trpc/router/_app'
+import { trpc } from '@/utils/trpc'
+import { prisma } from '@/server/db/client'
+import { NavArrow } from '@/components/navigation/NavArrow'
 import { ProjectRegistrationView } from '@/components/registration/ProjectRegistrationView'
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType
+} from 'next'
 
-export const NewProjectPage = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const spaces = await prisma.space.findMany({
+    include: {
+      BudgetBoxes: true
+    }
+  })
+
+  const params: Array<{
+    params: { spaceSlug: string; budgetBoxId: string }
+  }> = []
+  spaces.map(({ BudgetBoxes, slug }) =>
+    BudgetBoxes.map(({ id }) =>
+      params.push({
+        params: { spaceSlug: slug, budgetBoxId: id }
+      })
+    )
+  )
+  return {
+    paths: params,
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps = async (
+  context: GetStaticPropsContext<{ spaceSlug: string; budgetBoxId: string }>
+) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { session: null, prisma },
+    transformer: superjson
+  })
+  const spaceSlug = context.params?.spaceSlug as string
+  const budgetBoxId = context.params?.budgetBoxId as string
+
+  await ssg.space.getOneBySlug.prefetch({ slug: spaceSlug })
+  await ssg.budgetBox.getOne.prefetch({ id: budgetBoxId })
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      budgetBoxId,
+      spaceSlug
+    },
+    revalidate: 60
+  }
+}
+
+export const NewProjectPage = ({
+  budgetBoxId,
+  spaceSlug
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { data: space } = trpc.space.getOneBySlug.useQuery({
+    slug: spaceSlug
+  })
+  const { data: budgetBox } = trpc.budgetBox.getOne.useQuery({
+    id: budgetBoxId
+  })
+
+  const navArrowItems = [
+    { name: 'Home', path: '/' },
+    { name: space?.title, path: `/${spaceSlug}` },
+    { name: budgetBox?.title, path: `/${spaceSlug}/${budgetBoxId}` },
+    { name: 'New Project', path: `/${spaceSlug}/${budgetBoxId}/new` }
+  ]
+
   return (
     <>
       <Head>
         <title>Create Project</title>
       </Head>
+      <NavArrow items={navArrowItems} />
       <ProjectRegistrationView />
     </>
   )
--- src/pages/[spaceSlug]/new/pairwise.tsx
+++ src/pages/[spaceSlug]/new/pairwise.tsx
@@ -1,12 +1,64 @@
+import { createProxySSGHelpers } from '@trpc/react-query/ssg'
 import Head from 'next/head'
 import { BudgetBoxRegistrationView } from '@/components/registration/BudgetBoxRegistrationView'
+import { NavArrow } from '@/components/navigation/NavArrow'
+import { prisma } from '@/server/db/client'
+import { appRouter } from '@/server/trpc/router/_app'
+import superjson from '@/utils/superjson'
 
-export const NewBudgetBoxPage = () => {
+import { trpc } from '@/utils/trpc'
+import type {
+  InferGetStaticPropsType,
+  GetStaticPaths,
+  GetStaticPropsContext
+} from 'next'
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const spaces = await prisma.space.findMany()
+  return {
+    paths: spaces.map(({ slug }) => ({
+      params: { spaceSlug: slug }
+    })),
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps = async (
+  context: GetStaticPropsContext<{ spaceSlug: string }>
+) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { session: null, prisma },
+    transformer: superjson
+  })
+
+  const spaceSlug = context.params?.spaceSlug as string
+  await ssg.space.getOneBySlug.prefetch({ slug: spaceSlug })
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      spaceSlug
+    }
+  }
+}
+
+export const NewBudgetBoxPage = ({
+  spaceSlug
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { data: space } = trpc.space.getOneBySlug.useQuery({ slug: spaceSlug })
+
+  const navArrowItems = [
+    { name: 'Home', path: '/' },
+    { name: space?.title, path: `/${spaceSlug}` },
+    { name: 'New Pairwise', path: `/${spaceSlug}/new/pairwise` }
+  ]
   return (
     <>
       <Head>
         <title>Create Pairwise</title>
       </Head>
+      <NavArrow items={navArrowItems} />
       <BudgetBoxRegistrationView />
     </>
   )
