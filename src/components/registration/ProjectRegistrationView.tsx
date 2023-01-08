2cb7f72454bd154c06b73561e1aecea57902c07a
Vitor <marthendalnunes@gmail.com>
2023-01-08 11:35:43 -0300

    feat: add image upload component to new project

--- src/components/registration/ProjectRegistrationView.tsx
+++ src/components/registration/ProjectRegistrationView.tsx
@@ -1,5 +1,6 @@
 import * as Yup from 'yup'
 import { useRouter } from 'next/router'
+import { ImageUploader } from '@/components/inputs/ImageUploader'
 import { RegistrationLayout } from '@/components/registration/layout/RegistrationLayout'
 import { TextArea } from '@/components/inputs/TextArea'
 import { TextField } from '@/components/inputs/TextField'
@@ -8,12 +9,12 @@ import { useModal } from '@/hooks/useModal'
 import { trpc } from '@/utils/trpc'
 import { useFormNavigation } from '@/hooks/useFormNavigation'
 import { useSiwe } from '@/hooks/useSiwe'
+import { useImageUploader } from '@/hooks/useImageUploader'
 import type { FormikHelpers } from 'formik'
 
 interface Values {
   name: string
   owner: string
-  image: string
   url: string
   description: string
 }
@@ -22,6 +23,13 @@ const options = ['Create a project']
 
 export const ProjectRegistrationView = () => {
   const { selected, setSelected, handleNavigation } = useFormNavigation()
+  const {
+    image: projectImage,
+    setImage: setProjectImage,
+    imageFile: projectImageFile,
+    setImageFile: setProjectImageFile,
+    uploadImage
+  } = useImageUploader()
   const router = useRouter()
   const budgetBoxId = router.query.budgetBoxId as string
   const spaceSlug = router.query.spaceSlug as string
@@ -48,7 +56,6 @@ export const ProjectRegistrationView = () => {
   const initialValues = {
     name: '',
     owner: '',
-    image: '',
     url: '',
     description: ''
   }
@@ -57,7 +64,6 @@ export const ProjectRegistrationView = () => {
     Yup.object({
       name: Yup.string().max(40, 'Must be 40 characters max'),
       owner: Yup.string(),
-      image: Yup.string(),
       url: Yup.string().matches(
         /^((http|https):\/\/)?(www.)?(?!.*(http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+(\/)?.([\w\?[a-zA-Z-_%\/@?]+)*([^\/\w\?[a-zA-Z0-9_-]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/,
         'Website should be a valid URL'
@@ -68,9 +74,15 @@ export const ProjectRegistrationView = () => {
 
   const formList = [
     <>
+      <ImageUploader
+        height={250}
+        image={projectImage}
+        setFile={setProjectImageFile}
+        setImage={setProjectImage}
+        width={370}
+      />
       <TextField name="name" title="Name" />
       <TextField name="owner" title="Owner" />
-      <TextField maxLength={200} name="image" title="Image Link" />
       <TextField maxLength={200} name="url" title="Website" />
       <TextArea name="description" title="Description" />
     </>
@@ -78,19 +90,32 @@ export const ProjectRegistrationView = () => {
   const CurrentForms = ({ index }: { index: number }) => formList[index] || null
 
   const handleSubmit = async (
-    { name, owner, image, url, description }: Values,
+    { name, owner, url, description }: Values,
     { setSubmitting }: FormikHelpers<Values>
   ) => {
     if (selected === options.length - 1) {
       const signSuccess = await signIn()
 
       if (signSuccess && address) {
+        let imageUrl =
+          'https://user-images.githubusercontent.com/18421017/206027384-4869ad77-e635-4525-a5e8-e88eb8a5b206.png'
+
+        if (projectImageFile) {
+          const formData = new FormData()
+          formData.append('file', projectImageFile)
+          formData.append('upload_preset', 'pairwise-uploads')
+          const { data } = await uploadImage(formData)
+          if (data.secure_url) {
+            imageUrl = data.secure_url
+          }
+        }
+
         insertOneProjectMutation.mutate({
           owner,
           title: name,
           url,
           description,
-          image,
+          image: imageUrl,
           BudgetBoxes: [{ id: budgetBoxId }]
         })
         setIsModalOpen(true)
